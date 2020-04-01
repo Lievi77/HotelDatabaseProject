4861ba9fbfafef506004a9576a55e7624dd98a6e
Lievi77 <lguzm038@uottawa.ca>
2020-04-01 18:17:59 -0400

    Set up layout for data insertion

--- DatabaseBackup/PythonScript/connection.py
+++ DatabaseBackup/PythonScript/connection.py
@@ -1,19 +1,16 @@
+#written by Lev C. Guzman Aparicio 300038033 lguzm038@uottawa.ca 2020
 import csv
 import psycopg2
 
+#connection setup
+
 conn = psycopg2.connect("host= localhost dbname= project user = postgres password = cherokee77")
+
+########## Population of users schema ################
 
 #cursor for populating user_info table
 cur = conn.cursor()
-with open('bnb_NewYork_2019.csv', 'r') as f:
-    reader = csv.reader(f)
-    next(reader) # Skip the header row.
-    for row in reader:
-        cur.execute(
-        "INSERT INTO users VALUES (%s, %s, %s, %s,%s,%s,%s,%s,%s,%s,%s)",
-        row
-        )
-conn.commit()
+
 
 #cursor for populating guest_info table
 
@@ -22,4 +19,29 @@ conn.commit()
 #cursor for populating host_info table
 
 
+############ Population of transactions schema ################
 
+#cursor for populating transaction_info table 
+
+
+#cursor for populating the pricing table
+
+
+############ Population of properties schema ################
+
+#cursor for populating properties_info table
+
+
+#cursor for populating rental_agreement table
+
+
+############ Population of management schema ################
+
+# cursor for populating branch_info table
+
+
+#cursor for populating employee_info table
+
+
+
+#cursor for populating review_info table
---
+++ DatabaseBackup/PythonScript/datagenerator.py
@@ -0,0 +1,10 @@
+#written by Lev C. Guzman Aparicio 300038033 lguzm038@uottawa.ca 2020
+import csv
+import psycopg2
+
+#generates 100 instances of data for each of our tables. 
+
+####################### Generator for the users schema #################
+
+
+
