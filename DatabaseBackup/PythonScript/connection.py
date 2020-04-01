eaf3d010fe25591e9a5d7a345577d9c733c7f0ed
Lievi77 <lguzm038@uottawa.ca>
2020-04-01 18:04:41 -0400

    Python scripting set up

--- DatabaseBackup/PythonScript/connection.py
+++ DatabaseBackup/PythonScript/connection.py
@@ -3,9 +3,23 @@ import psycopg2
 
 conn = psycopg2.connect("host= localhost dbname= project user = postgres password = cherokee77")
 
+#cursor for populating user_info table
 cur = conn.cursor()
-with open('user_accounts.csv', 'r') as f:
-# Notice that we don't need the `csv` module.
-next(f) # Skip the header row.
-cur.copy_from(f, 'users', sep=',')
+with open('bnb_NewYork_2019.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader) # Skip the header row.
+    for row in reader:
+        cur.execute(
+        "INSERT INTO users VALUES (%s, %s, %s, %s,%s,%s,%s,%s,%s,%s,%s)",
+        row
+        )
 conn.commit()
+
+#cursor for populating guest_info table
+
+
+
+#cursor for populating host_info table
+
+
+
