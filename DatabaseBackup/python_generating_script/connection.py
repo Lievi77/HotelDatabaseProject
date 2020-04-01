120821b103113395e83ab686a7e706997dcc1674
Lievi77 <lguzm038@uottawa.ca>
2020-04-01 17:29:19 -0400

    Added python data insertion script

---
+++ DatabaseBackup/python_generating_script/connection.py
@@ -0,0 +1,11 @@
+import csv
+import psycopg2
+
+conn = psycopg2.connect("host= localhost dbname= project user = postgres password = cherokee77")
+
+cur = conn.cursor()
+with open('user_accounts.csv', 'r') as f:
+# Notice that we don't need the `csv` module.
+next(f) # Skip the header row.
+cur.copy_from(f, 'users', sep=',')
+conn.commit()
