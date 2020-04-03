8dcba5db40297cdb74211a93fcc2543319bd3836
Lievi77 <lguzm038@uottawa.ca>
2020-04-03 17:30:44 -0400

    renamed files

--- DatabaseBackup/PythonScript/data_insertor.py
+++ DatabaseBackup/PythonScript/data_insertor.py
@@ -1,44 +1,44 @@
-#written by Lev C. Guzman Aparicio 300038033 lguzm038@uottawa.ca 2020
+# written by Lev C. Guzman Aparicio 300038033 lguzm038@uottawa.ca 2020
 import csv
 import psycopg2
 
-#connection setup
+# connection setup
 
 conn = psycopg2.connect("host= localhost dbname= project user = postgres password = cherokee77")
 
 ########## Population of users schema ################
 
-#cursor for populating user_info, host_info and guest_info table
+# cursor for populating user_info, host_info and guest_info table
 # insertion in user_info, guarantees population of the other two
 
 cur = conn.cursor()
 
 with open('user_accounts.csv', 'r') as f:
     reader = csv.reader(f)
-    next(reader) # Skip the header row.
+    next(reader)  # Skip the header row.
     for row in reader:
         cur.execute(
-        "INSERT INTO users VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-        #must follow schema format
-        row
+            "INSERT INTO users VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+            # must follow schema format
+            row
         )
 
 conn.commit()
 
 ############ Population of transactions schema ################
 
-#cursor for populating transaction_info table 
+# cursor for populating transaction_info table
 
 
-#cursor for populating the pricing table
+# cursor for populating the pricing table
 
 
 ############ Population of properties schema ################
 
-#cursor for populating properties_info table
+# cursor for populating properties_info table
 
 
-#cursor for populating rental_agreement table
+# cursor for populating rental_agreement table
 
 
 ############ Population of management schema ################
@@ -46,8 +46,7 @@ conn.commit()
 # cursor for populating branch_info table
 
 
-#cursor for populating employee_info table
+# cursor for populating employee_info table
 
 
-
-#cursor for populating review_info table
+# cursor for populating review_info table
